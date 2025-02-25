766efb178da41206bdb8187fca380d2767857305
jjlamxpp <71970470+jjlamxpp@users.noreply.github.com>
2025-02-25 14:28:55 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'build', // Changed from 'dist' to 'build'
+    outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
       output: {
@@ -23,5 +24,6 @@ export default defineConfig({
   },
   server: {
     host: true,
+    port: 5173,
   },
 })
